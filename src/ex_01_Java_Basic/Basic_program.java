62557b34cf7f1f183ec73bbf634596b164b6f678
sbmishra150 <sbmishra150@gmail.com>
2025-06-25 08:28:15 +0530

    Initial commit

---
+++ src/ex_01_Java_Basic/Basic_program.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basic;
+
+public class Basic_program {
+    public static void main(String[] args) {
+        System.out.println("hello, i am running java program");
+    }
+}
---
+++ src/ex_01_Java_Basic/Lab002_basic_java.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basic;
+
+public class Lab002_basic_java {
+    public static void main(String[] args) {
+        System.out.println(2+2);
+    }
+}
