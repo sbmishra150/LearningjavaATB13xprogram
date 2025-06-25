887a28fdb38938d84997589e480d999262cff9ed
sbmishra150 <sbmishra150@gmail.com>
2025-06-25 14:57:20 +0530

    practice 10 jun 2025

---
+++ src/ex_01_Java_Basic/Table_of_9_.java
@@ -0,0 +1,16 @@
+package ex_01_Java_Basic;
+
+public class Table_of_9_ {
+    public static void main(String[] args) {
+        System.out.printf("9 X 1 = %d\n",9*1);
+        System.out.printf("9 X 2 = %d\n",9*2);
+        System.out.printf("9 X 3 = %d\n",9*3);
+        System.out.printf("9 X 4 = %d\n",9*4);
+        System.out.printf("9 X 5 = %d\n",9*5);
+        System.out.printf("9 X 6 = %d\n",9*6);
+        System.out.printf("9 X 7 = %d\n",9*7);
+        System.out.printf("9 X 8 = %d\n",9*8);
+        System.out.printf("9 X 9 = %d\n",9*9);
+        System.out.printf("9 X 10 = %d\n",9*10);
+    }
+}
